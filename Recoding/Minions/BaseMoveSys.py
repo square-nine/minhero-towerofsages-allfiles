ffa3541fed880d03fa1eb2bf11eb07b089fee2e4
square-nine <squarenineroblox@gmail.com>
2024-03-11 19:00:21 +0000

    Got rid of conversion ratio notes
    Since I now have the equations from the code, the document is useless now

--- Recoding/Minions/BaseMoveSys.py
+++ Recoding/Minions/BaseMoveSys.py
@@ -3,11 +3,24 @@
 #but essentially, this will be a place to put all of the modules re-coded
 
 import MoveHandler #proxy for handling special playbacks
+import random #doing random stuff
 
 
 #constants to be accessed everywhere, all in one dictionary!
 global consts
 consts = {}
+
+def CalcDamageOrHealing(damage, extraDamage, minionMax, level, param5=False):
+    '''Calculates the damage/healing/shield amount from minion stat + level, and move stats.
+       Param5 so far doesn't seem to have an effect, but set it as True for shields (for some reasons)
+       '''
+    #explanation of this equation:
+    # the first part configures the level to a multiplier
+    # the second part calculates the move's actual value to use, by including random chance
+    # the third part scales down the damage value and includes the minion's stats.
+    return (level * 3 / 5 + 2) * (damage + extraDamage * random.random()) * minionMax /3000
+
+
 
 def ApplyEffectsOfCurrentMove(currMinion, #the executing minion
                               currMove, #the move being executed
@@ -38,7 +51,7 @@ def ApplyEffectsOfCurrentMove(currMinion, #the executing minion
     #get common calculations
     _loc1_ = CalcDamageOrHealing(currMove.damage, currMove.AdditionalDamage, currMinion.maxATK, currMinion.level) #damage of move
     _loc2_ = CalcDamageOrHealing(currMove.healing, currMove.AdditionalHealing, currMinion.maxHeal, currMinion.level) #healing of move
-    _loc3_ = CalcDamageOrHealing(currMove.ShieldAmount, 0, currMinion.maxHeal, currMinion.level, False) #sheilding of move
+    _loc3_ = CalcDamageOrHealing(currMove.ShieldAmount, 0, currMinion.maxHeal, currMinion.level, False) #shielding of move
     #check if move is better due to "type resonance"
     if currMinion.type1 == currMove.type or currMinion.type2 == currMove.type:
         _loc1_ *= 1.1
@@ -56,6 +69,9 @@ def ApplyEffectsOfCurrentMove(currMinion, #the executing minion
                     loc9 += targets[loc10].redirectDamage
                     if currMove.damage >0 or currMove.AdditionalDamage > 0:
                         #play redirected animation on the target minion
-        else if the player has a minion at position loc10 and its health is above zero:
-            if that minion has redirect damage above zero:
-
+        elif StageState["player"][loc10] != None and StageState["player"][loc10].health > 0:
+            if StageState["player"][loc10].redirectDMG > 0:
+                loc8 = StageState["player"][loc10]
+                loc9 = StageState["player"][loc10].redirectDMG
+                if currMove.damage >0 or currMove.AdditionalDamage > 0:
+                    #play redirected animation on the target minion
